34c35b46733cc53babccfae4b87eca6182c21bfd
Marcel Dao <marcelnamdao@gmail.com>
2018-08-22 17:29:49 -0700

    Tic Tac Toe

--- tic-tac-toe/main.cpp
+++ tic-tac-toe/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <cstdlib>
 #include <chrono>
 #include <thread>
+#include "utils.h"
 using std::cout;
 using std::endl;
 using std::cin;
+using std::vector;
 using namespace std::chrono_literals;
 using namespace std::this_thread;
+
+void main()
+{
+	// 14 tabs, 3 spaces to center text
+	cout << "														   Tic-Tac-Toe" << endl;
+	sleep_for(1s);
+	ClearScreen();
+	cout << "" << endl;
+	cout << 
+R"(
+				 _     _    _
+				| |   | |  | |
+				| |   | |  | |
+				| |   | |  | |
+				| |_ _| |  | |
+				|  _ _  |  | |
+				| |   | |  | |
+				| |   | |  | |
+				| |   | |  | |
+				|_|   |_|  |_|)";
+	sleep_for(1s);
+}
